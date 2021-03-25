6c76b9f04f58e09a049620039c0e3a53e24ecb9c
Pramit Ghosh <pramitghosh@outlook.com>
2021-03-25 16:50:09 +0100

    Function L8_SR() for calculating surface reflectance by linear scaling of Level-2 Landsat-8 bands

---
+++ calcAlbedo.R
@@ -0,0 +1,23 @@
+library(jsonlite)
+
+albedo_coeffs_daSilva = c(0.300, 0.277, 0.233, 0.143, 0.036, 0.012)
+
+albedo_coeffs_grass_mult = c(2.153642, -2.242688, -0.520669,  0.622670,  0.129979, -0.047970,  0.152228)
+albedo_coeffs_grass_add = 0.058674
+
+L8_SR = function(L8.SR)
+{
+  L2_MTL_JSON_path = file("data/L8_C2/SR/LC08_L2SP_197024_20170614_20200903_02_T1_MTL.json")
+  L2_MTL = fromJSON(L2_MTL_JSON_path)
+  
+  sr_params = t(as.data.frame(L2_MTL$LANDSAT_METADATA_FILE$LEVEL2_SURFACE_REFLECTANCE_PARAMETERS))
+  
+  sr_coeffs_mult = as.numeric(sr_params[29:35, ])
+  sr_coeffs_add = as.numeric(sr_params[36:42, ])
+  
+  sr_scaled = L8.SR
+  for(i in 1:7)
+    sr_scaled[[i]] = (sr_scaled[[i]] * sr_coeffs_mult[i]) + sr_coeffs_add[i]
+  
+  sr_scaled
+}
