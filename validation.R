ce8994d6a0ab27eab1b5fc141d4972a03df76401
Pramit Ghosh <pramitghosh@outlook.com>
2021-03-31 01:14:18 +0200

    calculate daily ET for validation pts

--- validation.R
+++ validation.R
@@ -1,6 +1,8 @@
+library(sf)
+
 leo_coords = c(403873.8,	5759162)
 
-val_at_coords = function(image, coord_pair)
+val_at_coords = function(coord_pair, image)
 {
   coord_extent = c(coord_pair, coord_pair)
   coord_extent = coord_extent[order(c(1,3,2,4))]
@@ -15,6 +17,13 @@ val_at_coords = function(image, coord_pair)
 # leo_extents = leo_extents[order(c(1,3,2,4))]
 # e = extent(leo_extents)
 
-leo_val = val_at_coords(ET.24, leo_coords)
-
+leo_val = val_at_coords(image = ET.24, coord_pair = leo_coords)
 # Measured value on ground = 4.46 mm/day
+
+vpts = read_sf("results/pts.gpkg")
+pts_geom = st_geometry(vpts)
+ET_pts = lapply(X = pts_geom, FUN = function(x, img) val_at_coords(as.numeric(x), img), ET.24)
+val_results = as.data.frame(cbind(vpts$LULC, as.numeric(unlist(ET_pts))))
+val_results$V2 = as.numeric(val_results$V2)
+
+
