f3934ab315f46fbb31355fe483a16688fcfdb63a
Pramit Ghosh <pramitghosh@outlook.com>
2021-04-01 00:41:02 +0200

    added validation code to Markdown

--- validation.R
+++ validation.R
@@ -1,6 +1,6 @@
 library(sf)
 
-leo_coords = c(403873.8,	5759162)
+# leo_coords = c(403873.8,	5759162)
 
 val_at_coords = function(coord_pair, image)
 {
@@ -17,13 +17,13 @@ val_at_coords = function(coord_pair, image)
 # leo_extents = leo_extents[order(c(1,3,2,4))]
 # e = extent(leo_extents)
 
-leo_val = val_at_coords(image = ET.24, coord_pair = leo_coords)
+# leo_val = val_at_coords(image = ET.24, coord_pair = leo_coords)
 # Measured value on ground = 4.46 mm/day
 
-vpts = read_sf("results/pts.gpkg")
-pts_geom = st_geometry(vpts)
-ET_pts = lapply(X = pts_geom, FUN = function(x, img) val_at_coords(as.numeric(x), img), ET.24)
-val_results = as.data.frame(cbind(vpts$LULC, as.numeric(unlist(ET_pts))))
-val_results$V2 = as.numeric(val_results$V2)
+# vpts = read_sf("results/pts.gpkg")
+# pts_geom = st_geometry(vpts)
+# ET_pts = lapply(X = pts_geom, FUN = function(x, img) val_at_coords(as.numeric(x), img), ET.24)
+# val_results = as.data.frame(cbind(vpts$LULC, as.numeric(unlist(ET_pts))))
+# val_results$V2 = as.numeric(val_results$V2)
 
 
